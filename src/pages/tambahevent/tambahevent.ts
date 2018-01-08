d7e6cf9a9c979ee9a0e702331b68e56517ea7fac
inf4my <naufal.irfan.h@gmail.com>
2018-01-08 12:49:12 +0700

    Udah bisa nambah event

--- src/pages/tambahevent/tambahevent.ts
+++ src/pages/tambahevent/tambahevent.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
 import firebase from "firebase";
 import 'rxjs';
 
 //interface
 import { Event } from "../../data/event.interface";
+import { Toast } from 'ionic-angular/components/toast/toast';
 
 /**
  * Generated class for the TambaheventPage page.
@@ -22,7 +23,11 @@ import { Event } from "../../data/event.interface";
 })
 export class TambaheventPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public toastCtrl: ToastController
+    ) {
   }
 
   ionViewDidLoad() {
@@ -30,6 +35,7 @@ export class TambaheventPage {
   }
 
   submit(form: NgForm){
+    // var temp: Event
     //  temp.deskripsi = form.value.deskripsi
     //  temp.jdPendaftar = "0"
     //  temp.key = form.value.nama
@@ -38,19 +44,31 @@ export class TambaheventPage {
     //  temp.nPendaftar = "0"
     //  temp.penyelenggara = form.value.penyelenggara
     //  temp.tanggal = form.value.tgl
-    console.log(form.value.nama)
+    // console.log(form.value.deskripsi)
 
-    // firebase.database().ref('eventTable/'+ form.value.nama).set({
-    //   deskripsi: form.value.deskripsi,
-    //   jdPendaftar: "0",
-    //   key: form.value.key,
-    //   lokasi: form.value.tempat,
-    //   maks: form.value.maks,
-    //   nPendaftar: "0",
-    //   penyelenggara: form.value.penyelenggara,
-    //   tanggal: form.value.tgl,
-    //   waktu: form.value.wkt + "-"+ form.value.sls
-    // })
+    firebase.database().ref('eventTable/'+ form.value.nama).set({
+      deskripsi: form.value.deskripsi,
+      jdPendaftar: "0",
+      key: form.value.nama,
+      lokasi: form.value.tempat,
+      maks: form.value.maks,
+      nPendaftar: "0",
+      penyelenggara: form.value.penyelenggara,
+      tanggal: form.value.tgl,
+      waktu: form.value.wkt + "-"+ form.value.sls
+    })
+
+    this.presentToast()
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Event berhasil ditambahkan',
+      duration: 2000,
+      position: 'top'
+    });
+    toast.present();
+    this.navCtrl.pop()
   }
 
 }
