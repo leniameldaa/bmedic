649f1393bbc523c831b2738ebb66d97e50c168f9
naufal23 <naufal.irfan@yahoo.com>
2018-01-17 08:51:12 +0700

    Tampilin gambar event

--- src/pages/detail-event/detail-event.ts
+++ src/pages/detail-event/detail-event.ts
@@ -4,6 +4,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { Event } from "../../data/event.interface";
 
+import firebase from 'firebase';
+
 /**
  * Generated class for the DetailEventPage page.
  *
@@ -18,11 +20,26 @@ import { Event } from "../../data/event.interface";
 })
 export class DetailEventPage {
   event:any ={}
+  link:any
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     this.event = this.navParams.get("kiriman")
+    var storage = firebase.storage();
+    var storageRef = storage.ref();
+    var tangRef = storageRef.child(this.event.key+'.jpeg');
+    tangRef.getDownloadURL().then(function(url){
+      // Once we have the download URL, we set it to our img element
+      // this.link = url
+      var temp = document.getElementById("gambar")
+      temp.setAttribute("src", url) 
+      console.log(url)
+
+    }).catch(function(error) {
+      // If anything goes wrong while getting the download URL, log the error
+      console.log(error);
+      });
     console.log(this.event.key)
     //console.log('ionViewDidLoad DetailEventPage');
   }
